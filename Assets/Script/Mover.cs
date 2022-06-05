8a15a9fc4fc9fb75f29f1820b51f953b4c5e8af4
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-06-05 11:04:56 +0200

    Added animations to blend tree

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -14,6 +14,15 @@ public class Mover : MonoBehaviour
         {
             MoveToCursos();
         }
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
     }
 
     private void MoveToCursos()
