4b6d183065491e64bc697ac4c2ea03779bb7a48a
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2022-06-05 11:37:36 +0200

    Full basic movement implementation+ (#3)
    * Added knight mesh to player
    
    * Added animations to character
    
    * Added animations to blend tree
    
    * New folder structure

---
+++ Assets/Script/Movement/Mover.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            MoveToCursos();
+        }
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+    }
+
+    private void MoveToCursos()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
